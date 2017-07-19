a983cc1ceec9b88f6f0b7f12555f8d331be21f0e
Box <changleibox@sina.com>
2017-07-20 00:58:35 +0800

    修改二维码扫描

--- scanqrcode/src/main/java/me/box/library/scanqrcode/ImageUtils.java
+++ scanqrcode/src/main/java/me/box/library/scanqrcode/ImageUtils.java
@@ -1,118 +1,46 @@
 package me.box.library.scanqrcode;
 
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
 
 @SuppressWarnings("JavaDoc")
 class ImageUtils {
-    /**
-     * drawableToBitmap with white background
-     *
-     * @param drawable
-     * @return
-     */
-    static Bitmap drawableToBitmap(Drawable drawable) {
-        // 取 drawable 的长宽  
-        int w = drawable.getIntrinsicWidth();
-        int h = drawable.getIntrinsicHeight();
 
-        // 取 drawable 的颜色格式  
-        Bitmap.Config config = Bitmap.Config.ARGB_8888;
-        // 建立对应 bitmap  
-        Bitmap bitmap = Bitmap.createBitmap(w, h, config);
-        // 建立对应 bitmap 的画布  
-        Canvas canvas = new Canvas(bitmap);
-        // 白色底色   应对透明图  
-        Paint paint = new Paint();
-        paint.setColor(Color.WHITE);
-        canvas.drawRect(0, 0, w, h, paint);
-        drawable.setBounds(0, 0, w, h);
-        // 把 drawable 内容画到画布中  
-        drawable.draw(canvas);
-        return bitmap;
-    }
-
-    /**
-     * YUV420sp
-     *
-     * @param inputWidth
-     * @param inputHeight
-     * @param scaled
-     * @return
-     */
     static byte[] getYUV420sp(int inputWidth, int inputHeight, Bitmap scaled) {
         int[] argb = new int[inputWidth * inputHeight];
         scaled.getPixels(argb, 0, inputWidth, 0, 0, inputWidth, inputHeight);
-        byte[] yuv = new byte[inputWidth * inputHeight * 3 / 2];
-        encodeYUV420SP(yuv, argb, inputWidth, inputHeight);
-        // scaled.recycle();
-        return yuv;
+        return rgb2YCbCr420(argb, inputWidth, inputHeight);
     }
 
-    /**
-     * RGB转YUV420sp
-     *
-     * @param yuv420sp inputWidth * inputHeight * 3 / 2
-     * @param argb     inputWidth * inputHeight
-     * @param width
-     * @param height
-     */
-    private static void encodeYUV420SP(byte[] yuv420sp, int[] argb, int width, int height) {
-        // 帧图片的像素大小  
-        final int frameSize = width * height;
-        // ---YUV数据---  
-        int Y, U, V;
-        // Y的index从0开始  
-        int yIndex = 0;
-        // UV的index从frameSize开始  
-        int uvIndex = frameSize;
+    private static byte[] rgb2YCbCr420(int[] pixels, int width, int height) {
+        int len = width * height;
+        //yuv格式数组大小，y亮度占len长度，u,v各占len/4长度。
+        byte[] yuv = new byte[len * 3 / 2];
+        int y, u, v;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                //屏蔽ARGB的透明度值
+                int rgb = pixels[i * width + j] & 0x00FFFFFF;
+                //像素的颜色顺序为bgr，移位运算。
+                int r = rgb & 0xFF;
+                int g = (rgb >> 8) & 0xFF;
+                int b = (rgb >> 16) & 0xFF;
 
-        // ---颜色数据---  
-        // int a, R, G, B;
-        int R, G, B;
-        //  
-        int argbIndex = 0;
-        //  
+                //套用公式
+                y = ((66 * r + 129 * g + 25 * b + 128) >> 8) + 16;
+                u = ((-38 * r - 74 * g + 112 * b + 128) >> 8) + 128;
+                v = ((112 * r - 94 * g - 18 * b + 128) >> 8) + 128;
 
-        // ---循环所有像素点，RGB转YUV---  
-        for (int j = 0; j < height; j++) {
-            for (int i = 0; i < width; i++) {
-                // a is not used obviously
-                // a = (argb[argbIndex] & 0xff000000) >> 24;
-                R = (argb[argbIndex] & 0xff0000) >> 16;
-                G = (argb[argbIndex] & 0xff00) >> 8;
-                B = (argb[argbIndex] & 0xff);
-                //  
-                argbIndex++;
+                //调整
+                y = y < 16 ? 16 : (y > 255 ? 255 : y);
+                u = u < 0 ? 0 : (u > 255 ? 255 : u);
+                v = v < 0 ? 0 : (v > 255 ? 255 : v);
 
-                // well known RGB to YUV algorithm  
-                Y = ((66 * R + 129 * G + 25 * B + 128) >> 8) + 16;
-                U = ((-38 * R - 74 * G + 112 * B + 128) >> 8) + 128;
-                V = ((112 * R - 94 * G - 18 * B + 128) >> 8) + 128;
-
-                //  
-                Y = Math.max(0, Math.min(Y, 255));
-                U = Math.max(0, Math.min(U, 255));
-                V = Math.max(0, Math.min(V, 255));
-
-                // NV21 has a plane of Y and interleaved planes of VU each  
-                // sampled by a factor of 2  
-                // meaning for every 4 Y pixels there are 1 V and 1 U. Note the  
-                // sampling is every other  
-                // pixel AND every other scanline.  
-                // ---Y---  
-                yuv420sp[yIndex++] = (byte) Y;
-                // ---UV---  
-                if ((j % 2 == 0) && (i % 2 == 0)) {
-                    //  
-                    // yuv420sp[uvIndex++] = (byte) V;
-                    //  
-                    yuv420sp[uvIndex++] = (byte) U;
-                }
+                //赋值
+                yuv[i * width + j] = (byte) y;
+                yuv[len + (i >> 1) * width + (j & ~1)] = (byte) u;
+                yuv[len + +(i >> 1) * width + (j & ~1) + 1] = (byte) v;
             }
         }
+        return yuv;
     }
 } 
